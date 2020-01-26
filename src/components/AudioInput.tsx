5bcfda5c6cbfdc5ca779fdd88190e877c32730cf
Dmytro Izotov <izotov.dima@gmail.com>
2020-01-27 00:28:02 +0200

    Another fix

--- src/components/AudioInput.tsx
+++ src/components/AudioInput.tsx
@@ -4,22 +4,22 @@ interface AudioInput {}
 
 const AudioInput: React.FC<AudioInput> = () => {
   let src
+  if (typeof window !== 'undefined') {
+    let context = new AudioContext()
+    let analyser = context.createAnalyser()
+    let array = new Uint8Array(64)
 
-  let context = typeof window !== 'undefined' && new AudioContext()
-  let analyser = context.createAnalyser()
-  let array = new Uint8Array(64)
-
-  navigator.mediaDevices
-    .getUserMedia({ audio: true })
-    .then(stream => {
-      src = context.createMediaStreamSource(stream)
-      src.connect(analyser)
-    })
-    .catch(error => {
-      alert(error + 'OTKLONENO')
-    })
-  analyser.getByteFrequencyData(array)
-
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then(stream => {
+        src = context.createMediaStreamSource(stream)
+        src.connect(analyser)
+      })
+      .catch(error => {
+        alert(error + 'OTKLONENO')
+      })
+    analyser.getByteFrequencyData(array)
+  }
   return <div></div>
 }
 
